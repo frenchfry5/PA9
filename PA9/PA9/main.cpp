b51d9e2cb3de08002bec56fec794c186e74be1cb
frenchfry5 <fatthomasmap@gmail.com>
2025-04-18 11:18:56 -0700

    Fix Maybe

--- PA9/PA9/main.cpp
+++ PA9/PA9/main.cpp
@@ -1,35 +1,21 @@
 #include <SFML/Graphics.hpp>
-#include "Player.hpp"
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Platformer Test");
-    window.setFramerateLimit(60);
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
 
-    Player player({ 100.f, 100.f });
-
-    sf::RectangleShape ground;
-    ground.setSize({ 800.f, 100.f });
-    ground.setFillColor(sf::Color(100, 100, 100));
-    ground.setPosition(0.f, 500.f);
-
-    sf::Clock clock;
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
                 window.close();
         }
 
-        float deltaTime = clock.restart().asSeconds();
-
-        player.update(deltaTime);
-
-        window.clear(sf::Color::Black);
-        player.render(window);
-        window.draw(ground);
+        window.clear();
+        window.draw(shape);
         window.display();
     }
-
-    return 0;
 }
