e5557b7a2401de877f6569ba53ded721586b9e14
frenchfry5 <fatthomasmap@gmail.com>
2025-04-18 12:16:21 -0700

    Player Movement v1

--- PA9/PA9/Player.hpp
+++ PA9/PA9/Player.hpp
@@ -2,13 +2,48 @@
 #include "GameObject.hpp"
 
 class Player : public GameObject {
-private: 
-    sf::RectangleShape shape;
-    sf::Vector2f velocity;
-
-    const float moveSpeed = 200.0f; 
-    const float gravity = 800.0f;
-
 public:
+    Player(const sf::Vector2f& position) {
+        shape.setSize({ 50.f, 50.f });
+        shape.setFillColor(sf::Color::Green);
+        shape.setPosition(position);
+    }
+
+    void update(float deltaTime) override {
+        // up
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W)) {
+            shape.move({ 0, (vertVelocity * deltaTime) });
+        }
+        // left
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+            shape.move({ -(speed * deltaTime) , 0 });
+        }
+        // right
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+            shape.move({ speed * deltaTime , 0 });
+        }
+        
+        // gravity as in falling
+        vertVelocity += gravity;
+        shape.move({ 0, vertVelocity * deltaTime });
+    }
+
+    void render(sf::RenderWindow& window) override {
+        window.draw(shape);
+    }
+
+    sf::Vector2f getPosition() const override {
+        return shape.getPosition();
+    }
+
+    sf::FloatRect getBounds() const override {
+        return shape.getGlobalBounds();
+    }
+
+private:
+    sf::RectangleShape shape;
+    float speed = 200;
+    float vertVelocity = 0;
+    float gravity = 3;
 
 };
