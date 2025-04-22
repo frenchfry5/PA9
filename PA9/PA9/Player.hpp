f2f1378a2acee1db870563a0f842dad8af658c7d
frenchfry5 <fatthomasmap@gmail.com>
2025-04-21 21:33:08 -0700

    Fixed Collisions AGAIN

--- PA9/PA9/Player.hpp
+++ PA9/PA9/Player.hpp
@@ -13,16 +13,25 @@ public:
     void update(float deltaTime, const Map& map) override {
         // up
         if (isKeyPressed(Key::W) && isGrounded) {
+            // check if any collision above
             sf::FloatRect nextPos = shape.getGlobalBounds();
             nextPos.position.y -= jump * deltaTime;
+            // if there is no collision, then move player
             if (!map.isColliding(nextPos)) {
                 shape.move({ 0, -(jump * deltaTime) });
             }
         }
         // left
         if (isKeyPressed(Key::A)) {
+            // check if any collision to the left
             sf::FloatRect nextPos = shape.getGlobalBounds();
             nextPos.position.x -= speed * deltaTime;
+
+            //shrink it a little so it cant detect the ground
+            nextPos.position.y += 1;
+            nextPos.position.y -= 2;
+
+            // if there is no collision, then move player
             if (!map.isColliding(nextPos)) {
                 shape.move({ -(speed * deltaTime) , 0 });
             }
@@ -30,21 +39,33 @@ public:
         }
         // right
         if (isKeyPressed(Key::D)) {
+            // check if any collision to the right
             sf::FloatRect nextPos = shape.getGlobalBounds();
             nextPos.position.x += speed * deltaTime;
+
+            //shrink it a little so it cant detect the ground
+            nextPos.position.y += 1;
+            nextPos.position.y -= 2;
+            // if there is no collision, then move player
             if (!map.isColliding(nextPos)) {
                 shape.move({ speed * deltaTime , 0 });
             }
         }
-        vertVelocity += gravity;
+        
        
+        // if anything on the map is not colliding with the player, then the player must be in the air, so falling
+        // else the player must be on the ground and not falling
         if (!map.isColliding(shape.getGlobalBounds())) {
             // gravity as in falling
+            vertVelocity += gravity;
             shape.move({ 0, vertVelocity * deltaTime });
         }
         else {
             vertVelocity = 0;
-            isGrounded = true;
+
+            sf::FloatRect groundCheck = shape.getGlobalBounds();
+            groundCheck.position.y += 1.0;
+            isGrounded = map.isColliding(groundCheck);
         }
     }
     void render(sf::RenderWindow& window) override {
